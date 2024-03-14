bcf2c42e61d766715eb287e7a7a01cc0287deb95
rizmalik95 <126835997+rizmalik95@users.noreply.github.com>
2024-03-13 22:58:01 -0700

    small tweaks

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -15,10 +15,11 @@ export default function Start() {
   return (
     <div className="flex flex-col max-w-hh min-h-screen bg-slate-100 items-center mx-auto">
       <div className="container flex flex-col items-center gap-8 pt-10 min-w-96">
-        <h1 className="text-4xl font-bold">Generate Scaffolds</h1>
+        <h1 className="text-4xl font-bold">Get Curriculum-Aligned Instructional Scaffolds</h1>
+        <p className="text-md font-normal text-gray-600">To get started, provide the link of the Illustrative Mathematics lesson you're teaching</p>
         <InputForm onSubmitUrl={(url) => handleSubmit(url)} />
         <Results url={url} submitCount={submitCount} />
       </div>
     </div>
-  );
+  );  
 }
