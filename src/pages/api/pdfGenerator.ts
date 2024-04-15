ee596ab425a2a33ada572ef6ba76883f21f106d4
Ronak Malde <rmalde@stanford.edu>
2024-04-14 22:04:40 -0700

    anotehr try

--- src/pages/api/pdfGenerator.ts
+++ src/pages/api/pdfGenerator.ts
@@ -66,7 +66,7 @@ export default async function handler(
       res.status(200).json({ pdfUrl: publicUrlData.publicUrl });
     } catch (error) {
       console.error("Error generating pdf:", error);
-      res.status(500).json({ error: "Failed to generate pdf." });
+      res.status(500).json({ error: "Failed to generate pdf: "+ error});
     }
   } else {
     // Handle any non-POST requests
